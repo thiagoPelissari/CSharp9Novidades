1c274826eb8f078cd2fcaf92d3cd4e369b69c116
Thiago Pelissari <thiago.pelissari@b2egroup.com.br>
2020-12-30 19:11:23 -0300

    Initial Commit C# 9

---
+++ ConsoleApp1/ConsoleApp1/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Pessoa
+    {
+        //3 Initial Settters
+        public int Id { get; init; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp1;
+using System;
+
+//2 Top Levels calls
+ 
+string name = "Thiago";
+
+// 1 Is not
+if (name is not null)
+    Console.WriteLine("Não é Nulo!");
+else
+    Console.WriteLine("É Nulo!");
+
+
+Console.WriteLine($"O resultado é: {Add(5, 5)}");
+
+//4 Type shorthand instantiation - Atalho de instanciação de tipos
+Pessoa p = new() { Id = 1, Nome = "Thiago", SobreNome = "Pelissari" };
+
+p.SobreNome = "Pellizzari";
+
+//Relational <, >, >=, <=
+//Logical not, and, or
+
+int temp = 11;
+
+//5. Relational & Logical pattern matching in switch expression
+string resultadoTemp = temp switch
+{
+    > 0 and <= 10 => "Frio",
+    <= -5 => " Muito Congelante",
+    _ => "Desconhecido"
+
+};
+
+
+Console.WriteLine($"Temperatura: {resultadoTemp}");
+
+
+double Add(double x, double y)
+{
+    return x + y;
+}
